0d2cf1b3bceffdb95c12813450f23d83fa47d97e
Mahmoud Shokr <m.shokr@mcsoil.com>
2020-07-12 16:12:52 +0200

    base api data and install libraries

--- server.js
+++ server.js
@@ -2,18 +2,56 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+  
+  
+  /*
+  // GET route
+app.get('/all', sendData);
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+// POST route
+app.post('/add', callBack);
+
+function callBack(req,res){
+  res.send('POST received');
+}
+
+// POST an animal
+const data = [];
+
+app.post('/addAnimal', addAnimal);
+
+function addAnimal (req,res){
+    data.push(req.body);
+	console.log(data);
+};
+*/
--- website/app.js
+++ website/app.js
@@ -1,5 +1,39 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+openWeatherApiKey = '&appid=d3c5afe31b7352e8daff388941151d1f';
+//http://api.openweathermap.org/data/2.5/weather?q=cairo&appid=d3c5afe31b7352e8daff388941151d1f
+
+
+/*
+const postData = async ( url = '', data = {})=>{
+  console.log(data)
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      // console.log(newData);
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
+}
+postData('/addAnimal', {animal:'lion'});
+*/
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+function createApiUrl(baseURL,cityName,apiKey){
+	return baseURL + cityName + openWeatherApiKey;
+}
